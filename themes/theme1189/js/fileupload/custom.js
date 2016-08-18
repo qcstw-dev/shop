4c9ea4e73dd0605ebb1f587d88293f4e6b5ab37f
QCS18 <QCS18@PC-18.qcs.taiwan>
2016-08-18 16:34:02 +0800

    Layout maker: resize the picture uploaded if bigger than 600x600

--- themes/theme1189/js/fileupload/custom.js
+++ themes/theme1189/js/fileupload/custom.js
@@ -25,15 +25,15 @@ $(function () {
             $('.hidden-original-picture').attr("src", dataURL);
             
             if (file.preview.attributes.width.value <= 500 || file.preview.attributes.height.value <= 500) {
-            $.magnificPopup.open({
-                items: [{
-                    src: $('\
-                        <div class="popup">'+
-                            '<div class="text-center">The size of the file you uploaded will be too small for us to prepare your customized product, please upload a higher quality picture</div>'+
-                        '</div>'),
-                    type:'inline'
-                }]
-            });
+                $.magnificPopup.open({
+                    items: [{
+                        src: $('\
+                            <div class="popup">'+
+                                '<div class="text-center">The size of the file you uploaded will be too small for us to prepare your customized product, please upload a higher quality picture</div>'+
+                            '</div>'),
+                        type:'inline'
+                    }]
+                });
             }
             
             $('.slider-vertical').show();
@@ -43,10 +43,19 @@ $(function () {
             
             // Kick everything off with the target image
             resizeableImage($('.resize-image'));
+            
             if (file.preview.attributes.width.value > 600 || file.preview.attributes.height.value > 600) {
-                var ratioWidth = 600 / file.preview.attributes.width.value;
-                var ratioHeight = 600 / file.preview.attributes.height.value;
-                resizeImage(file.preview.attributes.width.value * (1-ratioWidth), file.preview.attributes.height.value * (1-ratioHeight));
+                if (file.preview.attributes.width.value > 600) {
+                    var newWidth = 600;
+                    var ratio = newWidth / parseInt(file.preview.attributes.width.value);
+                    var newHeight = parseInt(file.preview.attributes.height.value) * ratio;
+                } else if (file.preview.attributes.height.value > 600) {
+                    var newHeight = 600;
+                    var ratio = newHeight / parseInt(file.preview.attributes.height.value);
+                    var newWidth = parseInt(file.preview.attributes.width.value) * ratio;
+                    
+                }
+                resizeImage(newWidth, newHeight);
             }
             
             if ($('.file-error')) {
